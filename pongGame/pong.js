77a472f6f9287e809ccd6b2e9a18f04863271da5
nBidari <43047091+nBidari@users.noreply.github.com>
2019-02-13 20:02:36 -0500

    quick update to pong

--- pongGame/pong.js
+++ pongGame/pong.js
@@ -12,3 +12,51 @@ ctx.fillStyle = CANVAS_BACKGROUND_COLOUR
 ctx.strokeStyle = CANVAS_BORDER_COLOUR
 ctx.fillRect(0,0,gameCanvas.width, gameCanvas.height)
 ctx.strokeRect(0,0,gameCanvas.width, gameCanvas.height)
+
+//Other variables
+
+let scoreP1 = 0
+let scoreP2 = 0
+
+/*
+The functions I'm going to need:
+
+Main function
+	Timeout
+		DidSomeoneScore
+		MovePlayerOne
+		MovePlayerTwo
+		
+		moveBall
+	,5)
+
+Maybe some stuff later with different game scenes.
+	Start Scene
+	Game Scene
+	Restart Scene
+
+	*wispers* maybe once im competent I 
+	can add AI to play single player
+*/
+
+function main() {
+
+	if (didGameEnd()) return //checks if game ended and
+							 //returns blank (ending function)
+
+
+	setTimeout(function onTick() {
+		
+		main()
+	}, 5)
+}
+
+function movePaddles() {
+
+}
+
+function didGameEnd() {
+	const gameEnd = scoreP1 === 10 || scoreP2 === 10
+
+	if (gameEnd) return true
+}
