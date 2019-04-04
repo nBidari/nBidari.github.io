af4f424cf88a751ac0d16eb1088d674286106ea9
Nima Bidari <nima.bidari@nBidari.local>
2019-04-04 12:20:45 -0400

    update

--- nBidari.github.io/Work Record/UN_Sustainability_Goals/script.js
+++ nBidari.github.io/Work Record/UN_Sustainability_Goals/script.js
@@ -28,7 +28,25 @@
 // 	}
 // }
 
+// AIR QUALITY WIDGET
+
+//Base Code
+(function(w,d,t,f){  w[f]=w[f]||function(c,k,n){s=w[f],k=s['k']=(s['k']||(k?('&k='+k):''));s['c']=  
+c=(c  instanceof  Array)?c:[c];s['n']=n=n||0;L=d.createElement(t),e=d.getElementsByTagName(t)[0];  
+L.async=1;L.src='//feed.aqicn.org/feed/'+(c[n].city)+'/'+(c[n].lang||'')+'/feed.v1.js?n='+n+k;  
+e.parentNode.insertBefore(L,e);  };  })(  window,document,'script','_aqiFeed'  );
+
+//Calling object
+_aqiFeed({    
+  display:"%details",  
+  container:"airQualityApi",    
+  city:"beijing"  
+});  
+
+
+
 var footer;
+showActivity(1);
 
 window.onload = function() {
 	footer = document.getElementById("footer")
@@ -37,4 +55,17 @@ window.onload = function() {
 
 function turnOffAnimation() {
 	footer.style.webkitAnimationPlayState = "paused";
+}
+
+
+/*Activity Bar*/
+function showActivity(n) {
+	let x = document.getElementsByClassName("activity");
+	if (n == 0) {
+		x[0].style.display = 'none';
+		x[1].style.display = 'block';
+	}else {
+		x[1].style.display = 'none';
+		x[0].style.display = 'block';
+	}
 }
