f575b5b1064d1f18f18fe7132bbebdfd75db5b40
nBidari <43047091+nBidari@users.noreply.github.com>
2019-04-07 15:28:56 -0400

    Unit 2 Update
    HUGE Update:
    
    Air Quality API is now nearly finished. Did all scripting and dropdowns for it, and bug tested. Only CSS left now.
    
    Footer CSS update.
    
    general CSS updates:
    removed margin colors for AQI container and dropdowns.

--- nBidari.github.io/Work Record/UN_Sustainability_Goals/AirQualityApi.js
+++ nBidari.github.io/Work Record/UN_Sustainability_Goals/AirQualityApi.js
@@ -32,3 +32,60 @@
 
 */
 
+// AIR QUALITY WIDGET
+var chosenLang, chosenCity;
+var cityValues = ['brisbane', 'melbourne', 'sydney', 'vienna', 'calgary', 'toronto', 'montreal','baoding', 'beijing', 'changchun', 'changsha', 'chengdu', 'dongguan', 'guangzhou', 'hangzhou', 'hongkong', 'shanghai', 'shenzhen','prague', 'cairo', 'giza', 'paris', 'berlin', 'newdelhi', 'jakarta', 'isfahan', 'tehran', 'milan', 'rome', 'hiroshima', 'kyoto', 'tokyo', 'yokohama', 'busan', 'seoul','moscow', 'barcelona', 'madrid', 'london', 'chigaco', 'philadelphia']
+var cityText = titleMaker(cityValues);
+var langDropdown = document.getElementById("languageDropdown");
+var cityDropdown = document.getElementById("airQualityDropdown");
+
+langDropdown.addEventListener("change", function() {
+	chosenLang = langDropdown.value;
+	aqiChange(chosenLang, chosenCity);
+});
+
+cityDropdown.addEventListener("change", function() {
+	chosenCity = cityDropdown.value;
+	aqiChange(chosenLang, chosenCity);
+
+	console.log(cityDropdown.value)
+});
+
+window.onload = function() {
+	for (var i = 0; i<cityValues.length; i++) {
+		var option = document.createElement('option');
+		option.text = cityText[i];
+		option.value = cityValues[i];
+		cityDropdown.add(option, 0);
+	}
+}
+
+
+function aqiChange(language, chosenCity) {
+	_aqiFeed({
+		display: '%details',
+		container:"airQualityApi",
+		lang: language,
+		city: chosenCity
+		// callback:function(aqi) {
+		// 	console.log(aqi);
+		// 	}
+	});
+}
+
+//Base Code
+(function(w,d,t,f){  w[f]=w[f]||function(c,k,n){s=w[f],k=s['k']=(s['k']||(k?('&k='+k):''));s['c']=  
+c=(c  instanceof  Array)?c:[c];s['n']=n=n||0;L=d.createElement(t),e=d.getElementsByTagName(t)[0];  
+L.async=1;L.src='//feed.aqicn.org/feed/'+(c[n].city)+'/'+(c[n].lang||'')+'/feed.v1.js?n='+n+k;  
+e.parentNode.insertBefore(L,e);  };  })(  window,document,'script','_aqiFeed'  );
+
+//Calling object
+// _aqiFeed({    
+// 	display:"%details",
+// 	container:"airQualityApi",
+// 	city: chosenCityName,
+// 	callback:function(aqi) {
+// 		console.log(aqi);
+// 	}
+// });
+
--- nBidari.github.io/Work Record/UN_Sustainability_Goals/script.js
+++ nBidari.github.io/Work Record/UN_Sustainability_Goals/script.js
@@ -28,21 +28,6 @@
 // 	}
 // }
 
-// AIR QUALITY WIDGET
-
-//Base Code
-(function(w,d,t,f){  w[f]=w[f]||function(c,k,n){s=w[f],k=s['k']=(s['k']||(k?('&k='+k):''));s['c']=  
-c=(c  instanceof  Array)?c:[c];s['n']=n=n||0;L=d.createElement(t),e=d.getElementsByTagName(t)[0];  
-L.async=1;L.src='//feed.aqicn.org/feed/'+(c[n].city)+'/'+(c[n].lang||'')+'/feed.v1.js?n='+n+k;  
-e.parentNode.insertBefore(L,e);  };  })(  window,document,'script','_aqiFeed'  );
-
-//Calling object
-_aqiFeed({    
-  display:"%details",  
-  container:"airQualityApi",    
-  city:"beijing"  
-});  
-
 
 
 var footer;
@@ -61,11 +46,24 @@ function turnOffAnimation() {
 /*Activity Bar*/
 function showActivity(n) {
 	let x = document.getElementsByClassName("activity");
-	if (n == 0) {
+	if (n == 0) { //Ambient Air Pollution is showm
 		x[0].style.display = 'none';
 		x[1].style.display = 'block';
-	}else {
-		x[1].style.display = 'none';
+		console.log("Show Ambient Air")
+	}else {	//AQI is showm
+		x[1].style.display = 'flex';
 		x[0].style.display = 'block';
+		console.log("Show AQI")
 	}
+
+	// console.log("it worked!")
+}
+
+function titleMaker(array) {
+	var newArray = [];
+	array.forEach(function(element) {
+		newArray.push(element.charAt(0).toUpperCase() + element.slice(1));
+	});
+	console.log(newArray);
+	return newArray;
 }
