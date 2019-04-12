027a1789b350a3f00823e180ad00a625d32a3504
Nima Bidari <nima.bidari@nBidari.local>
2019-04-12 15:32:17 -0400

    ambient air graph update

--- nBidari.github.io/Work Record/UN_Sustainability_Goals/ambientAirGraph.js
+++ nBidari.github.io/Work Record/UN_Sustainability_Goals/ambientAirGraph.js
@@ -40,41 +40,53 @@
 var data = [[],
 			[]];
 
-let colourList = ["#0D2BFF", '#0C61E8', '#00A7FF', '#0CD5E8', '#0DFFD1'];
+let colourList = ["#FF8F61", '#FF1701', '#FFE70A', '#ffffff', '#00FFEB', '#FF00FC', '#000000'];
 
 var staticUrl = 'https://raw.githubusercontent.com/nBidari/nBidari.github.io/master/nBidari.github.io/Work%20Record/UN_Sustainability_Goals/data/ambientAir-householdAir.json';
 
 	$.getJSON(staticUrl, function(JSONdata) {
 			for (var i=0; i<14; i+=2) {
-				data[0].push(JSONdata.fact[i].Value)
+				data[0].push(parseInt(JSONdata.fact[i].Value))
 				data[1].push(JSONdata.fact[i].dims.REGION)
 			}
 		console.log(data)
 	});
 
 function setup() {
+	const barMargin = 40; //Setting constants
+	const barWidth = 40;
+	const borderRadius = 15;
 
-	var width = 600, // canvas width and height
-		height = 300,
-		margin = 20,
-		w = width - 2 * margin, // chart area width and height
-		h = height - 2 * margin;
 
-	var barWidth =  (h / data.length) * 0.8; // width of bar
-	var barMargin = (h / data.length) * 0.2; // margin between two bars
-
-	var canvas = createCanvas(width, height);
+	var canvas = createCanvas(600,300);
 	canvas.parent('ambientAirGraph');
 
-	background('#0c69ff')
-
+	background('#70a7ff');
 	fill(0);
 
-	for (var i=0; i<7; i++) {
-		translate(i* (barWidth + barMargin),0);
-		rect(0, 0, barWidth, data[0][i]*2);
+
+	for (var i=0; i<7; i++) { //Creating the Graph
+		let dblData = (data[0][i] * 2); //Doubling data so that it seems bigger
+		let x = barMargin /*Instance of the margin*/ +(barMargin+barWidth)*i;
+		let y = 300-dblData;
+
+		push(); //Push and Pop create a bubble around styles (nothing that happens in 
+			//between affects anything outside. Works globally)
+
+		noStroke();
+		fill(colourList[i%7]);
+
+		//rect(x,y, width, height, tl-border, tr-border, bl-border, br-border);
+		rect(x, y, barWidth, dblData, borderRadius, borderRadius, 0,0);
+		pop();
+		if(i==6) { //For the global data
+			fill(255);
+		}
+
+		textFont('Helvetica');
+		text(data[0][i], x+10,y+20);
+
 	}
-	rect(30, 300-96.7, 50, 96.7);
 }
 
 
