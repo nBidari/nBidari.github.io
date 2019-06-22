b3d5d5e890e6a7a675f8eed02f68a8f33c73d412
nBidari <43047091+nBidari@users.noreply.github.com>
2019-06-22 16:46:28 -0400

    google auth updat
    stylized login button
    
    added logout function
    added auto displayname update
    
    worked out some edge cases
    
    organized the files a bit

--- public/app.js
+++ public/app.js
@@ -1,29 +1,71 @@
-document.addEventListener("DOMContentLoaded", event => {
+var loggedIn = false
 
-	var firebaseConfig = {
-		apiKey: "AIzaSyCwluasbfjPrJs1NpYOeRqXoKPh2MzxyUM",
-		authDomain: "digitrawebsite.firebaseapp.com",
-		databaseURL: "https://digitrawebsite.firebaseio.com",
-		projectId: "digitrawebsite",
-		storageBucket: "digitrawebsite.appspot.com",
-		messagingSenderId: "803896198398",
-		appId: "1:803896198398:web:e4d55502dfe179fb"
-	};
-	
-	// Initialize Firebase
-	firebase.initializeApp(firebaseConfig);
-
-	const app = firebase.app();
-})
-
-function googleLogin() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth().signInWithPopup(provider)
+const loginButton = document.getElementById("loginButton")
 
 
-            .then(result => {
-                const user = result.user
-                console.log("Welcome " + user.displayName)
-            })
+var firebaseConfig = {
+	apiKey: "AIzaSyCwluasbfjPrJs1NpYOeRqXoKPh2MzxyUM",
+	authDomain: "digitrawebsite.firebaseapp.com",
+	databaseURL: "https://digitrawebsite.firebaseio.com",
+	projectId: "digitrawebsite",
+	storageBucket: "digitrawebsite.appspot.com",
+	messagingSenderId: "803896198398",
+	appId: "1:803896198398:web:e4d55502dfe179fb"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const app = firebase.app();
+
+const googleLogin = () => {
+	const provider = new firebase.auth.GoogleAuthProvider();
+	var currentUser = firebase.auth().currentUser;
+
+	if (currentUser) {
+		googleLogout()
+	}else {
+		firebase.auth().signInWithPopup(provider)
+
+
+			.then(result => {
+				const user = result.user
+				console.log("Welcome " + user.displayName)
+
+				constdisplayName = user.displayName
+			})
+	}
+}
+
+//Updates display name at the top right every time user logs in or out
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) { //USER SIGNED IN
+		loginButton.innerHTML = user.displayName.toUpperCase()
+		loggedIn = true //Used in database stuff auth().currentUser
+		                //Used in other scenarios       
+
+	} else { //USER NOT SIGNED IN
+		loginButton.innerHTML = "SIGN IN"
+		loggedIn = false;
+	}
+});
+
+//LOGOUT
+const googleLogout = () => {
+
+	//Signout Consent
+	if (confirm("Are you sure you would like to logout?")) {
+		firebase.auth().signOut().then(function() {
+			// Sign-out successful.
+			console.log("Signout successful.")
+		}, function(error) {
+			// An error happened.
+			console.log("ERROR ERROR ERROR")
+			console.log("Signout failed. Please retry.")
+		});
+	}else {
+		// An error happened.
+			console.log("ERROR ERROR ERROR")
+			console.log("Signout failed. Please retry.")
+	}
 }
