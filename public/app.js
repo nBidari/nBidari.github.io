2ea7d8d559b7d2263996f0996383889f7757b384
nBidari <43047091+nBidari@users.noreply.github.com>
2019-06-22 16:13:12 -0400

    auth update

--- public/app.js
+++ public/app.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", event => {
 
-    const app = firebase.app();
+	var firebaseConfig = {
+		apiKey: "AIzaSyCwluasbfjPrJs1NpYOeRqXoKPh2MzxyUM",
+		authDomain: "digitrawebsite.firebaseapp.com",
+		databaseURL: "https://digitrawebsite.firebaseio.com",
+		projectId: "digitrawebsite",
+		storageBucket: "digitrawebsite.appspot.com",
+		messagingSenderId: "803896198398",
+		appId: "1:803896198398:web:e4d55502dfe179fb"
+	};
+	
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig);
+
+	const app = firebase.app();
 })
 
 function googleLogin() {
@@ -11,7 +24,6 @@ function googleLogin() {
 
             .then(result => {
                 const user = result.user
-                document.write('Hello ${user.displayName}')
-                console.log(user.displayName)
+                console.log("Welcome " + user.displayName)
             })
 }
